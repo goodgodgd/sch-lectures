4da24d1cec1145e83af1b20b012dbedd145849e2
goodgodgd <goodgodgd@yonsei.ac.kr>
2020-11-14 03:28:15 +0900

    update match features code

--- Vision3D/MatchFeatures/feature.hpp
+++ Vision3D/MatchFeatures/feature.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cassert>
 #include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
 
@@ -6,73 +7,150 @@ typedef cv::Ptr<cv::Feature2D> FeaturePtr;
 typedef cv::Ptr<cv::DescriptorMatcher> MatcherPtr;
 
 
+struct DetectResult
+{
+    const std::string name;
+    cv::Mat image;
+    const std::vector<cv::KeyPoint>& keypts;
+    cv::Mat descriptors;
+};
+
 // Detector holds keypoint detector, descriptor computer and their results
 class Detector
 {
+    FeaturePtr feature;
     std::string name;
     cv::Mat image;
     std::vector<cv::KeyPoint> keypts;
     cv::Mat descriptors;
 
 public:
-    Detector(const std::string name, FeaturePtr feature) {}
-    static Detector Factory(const std::string name) {}
+    Detector(const std::string _name, FeaturePtr _feature)
+    {}
+    
+    static Detector Factory(const std::string name)
+    {}
+    
+    void DetectAndCompute(cv::Mat image)
+    {}
+    
+    DetectResult getResult()
+    {
+        return {name, image, keypts, descriptors};
+    }
 
+    std::string GetName() { return name; }
 };
-// factory contructor
 
-// detectAndCompute
+
+struct MatcherResult
+{
+    const std::string name;
+    const std::vector<cv::DMatch>& matches;
+};
 
 
 class Matcher
 {
+    MatcherPtr matcher;
     std::string name;
+    std::vector<cv::DMatch> matches;
+    const int minMathces = 10;
 
 public:
-    Matcher(const std::string name, MatcherPtr matcher) {}
-    static Matcher Factory(const std::string name) {}
-    void SetRefFeatures(std::vector<cv::KeyPoint>& keypts, cv::Mat descriptors)
+    Matcher(const std::string _name, MatcherPtr _matcher)
     {}
     
+    static Matcher Factory(const std::string name)
+    {}
+    
+    std::vector<cv::DMatch>& MatchDescriptors(cv::Mat referDesc, cv::Mat inputDesc)
+    {}
+    
+    static float& AcceptRatio()
+    {
+        static float acceptRatio = 0.5f;
+        return acceptRatio;
+    }
+
+    MatcherResult getResult()
+    {
+        return {name, matches};
+    }
 };
-// factory contructor
-
-// match
-
-// drawMatches
-
 
 
 class MatchHandler
 {
-    std::vector<Detector> refer_dets;
-    std::vector<Detector> input_dets;
-    float inlierRatio;
-    int minMatches;
+    std::vector<Detector> referDets;
+    std::vector<Detector> inputDets;
+    std::vector<Matcher> matchers;
+    float acceptRatio;
 
 public:
     // create feature detectors and matchers depending on string inputs
-    // set reference image
     MatchHandler(const std::vector<std::string> features, 
-                 const std::vector<std::string> matchers, 
-                 cv::Mat refimg)
+                 const std::vector<std::string> matchers)
+                 : acceptRatio(0.5f)
     {
+        assert(features.size() == matchers.size());
         for(const std::string& feat : features)
-            refer_dets.push_back( Detector::Factory(feat) );
+            referDets.push_back( Detector::Factory(feat) );
         for(const std::string& feat : features)
-            input_dets.push_back( Detector::Factory(feat) );
-        for(const std::string& feat : features)
-            input_dets.push_back( Detector::Factory(feat) );
+            inputDets.push_back( Detector::Factory(feat) );
+        for(const std::string& match : matchers)
+            matchers.push_back( Matcher::Factory(match) );
     }
 
-    // detect features on reference image for all feature types
-    void SetRefImage(cv::Mat refimg) {}
+    // detect features and compute descriptors on reference image for all feature types
+    void SetRefImage(cv::Mat refimg)
+    {
+        for(auto& det: referDets)
+            det.DetectAndCompute(refimg);
+    }
 
-    //
+    // detect features and compute descriptors on input image for all feature types
+    // match input descriptors with reference descriptors
+    void MatchImage(cv::Mat inpimg)
+    {
+        for(auto& det: inputDets)
+            det.DetectAndCompute(inpimg);
+        
+        for(size_t i=0; i<matchers.size(); i++)
+        {
+            matchers[i].MatchDescriptors(referDets[i].getResult().descriptors, 
+                                         inputDets[i].getResult().descriptors);
+        }
+    }
+
+    // change minimum inlier ratio in Matcher class
+    void ChangeAcceptRatio(float change)
+    {
+        acceptRatio += change;
+        acceptRatio = std::max(std::min(acceptRatio, 1.f), 0.f);
+        Matcher::AcceptRatio() = acceptRatio;
+    }
+
+    // draw match
+    cv::Mat DrawMatchResult(int maxHeight=1000)
+    {
+        std::vector<cv::Mat> resultImgs;
+        for(size_t i=0; i<matchers.size(); i++)
+        {
+            cv::Mat result = DrawSingleResult(
+                referDets[i].getResult(), inputDets[i].getResult(), matchers[i].getResult()
+            );
+            resultImgs.push_back(result);
+        }
+        cv::Mat stackedResult;
+        cv::vconcat(resultImgs, stackedResult);
+        // if(stackedResult.rows > maxHeight)
+            // cv::resize()
+        return stackedResult;
+    }
+
+    cv::Mat DrawSingleResult(DetectResult refDet, DetectResult inpDet, MatcherResult match)
+    {
+    }
 
 };
-
-// detectAndMatch
-// float acceptRatio
-
-
--- Vision3D/MatchFeatures/main.cpp
+++ Vision3D/MatchFeatures/main.cpp
@@ -1,10 +1,46 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include "feature.hpp"
+
 
 int main()
 {
-    cv::Mat img(100, 100, CV_8UC3, 255);
-    cv::imshow("image", img);
-    cv::waitKey();
+    std::cout << "Press 'r' to change reference frame," << std::endl
+            << "'u' to increase min inlier ratio," << std::endl
+            << "'d' to decrease min inlier ratio," << std::endl
+            << "and 'q' to quit." << std::endl;
+
+    cv::VideoCapture cap(0);
+    if(!cap.isOpened())
+        return -1;
+    
+    cv::Mat frame;
+    cap >> frame;
+
+    MatchHandler matcher({"sift", "surf", "orb"}, {"bf", "flann", "flann"});
+    matcher.SetRefImage(frame);
+
+    while(1)
+    {
+        cap >> frame;
+        matcher.MatchImage(frame);
+
+        int key = cv::waitKey(10);
+        if(key==int('r') || key==int('R'))
+        {
+            std::cout << "change reference image" << std::endl;
+            matcher.SetRefImage(frame);
+        }
+        else if(key==int('u') || key==int('U'))
+            matcher.ChangeAcceptRatio(0.1f);
+        else if(key==int('d') || key==int('D'))
+            matcher.ChangeAcceptRatio(-0.1f);
+        else if(key==int('q') || key==int('Q'))
+            break;
+        
+        cv::Mat result = matcher.DrawMatchResult();
+        cv::imshow("matches", result);
+        cv::waitKey(10);
+    }
     return 0;
 }
